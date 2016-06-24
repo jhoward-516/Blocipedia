ce233305bc42ce4bcc4a135ebb3641d146907b0a
John Howard <longhornjoho@gmail.com>
2016-06-24 01:52:45 +0000

    private wiki integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,17 @@ class User < ActiveRecord::Base
   def default_role
     self.role ||= :standard
   end
+  
+  def downgrade
+    self.standard!
+    customer = Stripe::Customer.retrieve(self.stripe_id)
+    Stripe::Subscription.retrieve(customer.subscriptions.data.first.id).delete if customer.subscriptions.data.first
+    self.make_wikis_public
+  end
+  
+  def make_wikis_public
+    self.wikis.where(private: true).each do |wiki|
+      wiki.update_attributes(private: false)
+    end
+  end
 end
