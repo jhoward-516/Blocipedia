10a5076422ea66e7a0e5bafb8c1dc297d17c31d2
John Howard <longhornjoho@gmail.com>
2016-07-06 04:18:31 +0000

    collamorator complete and updated form views

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,31 +1,42 @@
 class CollaboratorsController < ApplicationController
     
     def create
-        @user = User.find_by!(params[:email])
-        @wiki = Wiki.find_by(params[:id])
+        user = User.where(email: params[:email]).first
+        wiki = Wiki.find(params[:wiki_id])
         
         # no user, raise error and redirect
-        rescue ActiveRecord::RecordNotFound
-        redirect_to(edit_wiki_path(@wiki), :error => 'User not found')
+        if user.nil?
+            flash[:error] = "User with #{params[:email]} was not found"
         
         # user is self, raise error and redirect
-        if @user.id == current_user.id
-            redirect_to(edit_wiki_path(@wiki), :alert => 'Cannot add self as collaborator')
+        elsif user.id == current_user.id
+            flash[:alert] = 'Cannot add self as collaborator'
         
         # user AND collaborators, raise error and redirect
-        elsif @wiki.users.include?(@user)
-            redirect_to(edit_wiki_path(@wiki), :error => 'User already exists as a collaborator')
+        elsif wiki.users.include?(user)
+            flash[:error] = "User with email #{params[:email]} already exists as a collaborator"
                 
         # user and no collaborator, create collaborator and populate message and redireect
-        elsif
-            redirect_to(edit_wiki_path(@wiki), :alert => 'Collaborator has been added')
+        else 
+            if Collaborator.create(user: user, wiki: wiki)
+                flash[:alert] = 'Collaborator has been added'
         
-        else
-            flash[:error] = "Error - Collaborator could not be added." 
-            redirect_to edit_wiki_path(@wiki) 
+            else
+                flash[:error] = "Error - Collaborator could not be added." 
+            end
         end
+        
+        redirect_to edit_wiki_path(wiki)
     end
     
     def destroy
+        collaborator = Collaborator.find(params[:id])
+        wiki = Wiki.find(params[:wiki_id])
+        
+        collaborator.delete
+        
+        flash[:alert] = "Collaborator has been removed"
+        
+        redirect_to edit_wiki_path(wiki)
     end
 end
